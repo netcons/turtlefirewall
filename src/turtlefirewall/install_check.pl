7f9c69c7ee374376378cc4e564eea412c2af5fed
netcons <git@ncc.co.za>
2026-05-08 08:00:35 +0200

    Cleanup : Rework comments.

--- src/turtlefirewall/install_check.pl
+++ src/turtlefirewall/install_check.pl
@@ -1,4 +1,12 @@
-# install_check.pl
+#!/usr/bin/perl
+
+#======================================================================
+# Turtle Firewall webmin module
+#
+# Copyright (c) 2001-2026 Andrea Frigido <andrea@frisoft.it>
+# You may distribute under the terms of either the GNU General Public
+# License
+#======================================================================
 
 require './turtlefirewall-lib.pl';
 
